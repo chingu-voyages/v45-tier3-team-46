486d2783e9b07d8a9ce1ddac36620323ba3e007b
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-20 16:40:04 -0500

    created async method for adding user into planetscale DB using prisma

--- chingu-auction-app/app/api/signup/route.ts
+++ chingu-auction-app/app/api/signup/route.ts
@@ -1,10 +1,25 @@
-// import { prisma } from "@/lib/prisma";
-//const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
+import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
+
+const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   const { username, email, password } = await req.json()
-  const user = { username, email }
 
-  return NextResponse.json(user)
+  const new_hash = await bcrypt.hash(password, 10)
+
+  const new_user = await prisma.user.create({
+    data: {
+      username: username,
+      email: email,
+      password: new_hash
+    }
+  })
+
+  console.log(new_user)
+
+  return NextResponse.json(new_user?.username)
 }
+
+
