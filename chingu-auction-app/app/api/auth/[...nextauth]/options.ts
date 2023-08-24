24ae9bea9cae16dccb5aa474fc1254b3718aed82
Curt <121463645+curtwl@users.noreply.github.com>
2023-08-23 22:27:02 -0500

    Implement db sessions for credentials logins

--- chingu-auction-app/app/api/auth/[...nextauth]/options.ts
+++ chingu-auction-app/app/api/auth/[...nextauth]/options.ts
@@ -5,8 +5,10 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { type Adapter } from "@auth/core/adapters"
 import { PrismaClient } from '@prisma/client'
 const bcrypt = require('bcrypt')
+import { randomUUID } from "crypto"
 
 const prisma = new PrismaClient()
+const maxAge = 30 * 24 * 60 * 60
 
 export const options: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -56,20 +58,34 @@ export const options: NextAuthOptions = {
       },
       async authorize(credentials) {
         try {
-          const user = await prisma.user.findUnique({
+          user = await prisma.user.findUnique({
             where: {
-              username: credentials.username,
+              username: credentials?.username,
             },
           })
                 
-          const passwordIsCorrect = await bcrypt.compare(credentials?.password, user.password)
+          const passwordIsCorrect = await bcrypt.compare(credentials?.password, user?.password)
 
-          if (passwordIsCorrect && credentials?.username === user.username) {
-            console.log(user, 'user found in db')
-            return user
-          } else {
-            console.log('OAuth')
+          if (!passwordIsCorrect || !(credentials?.username === user?.username)) {
+            console.log(user, 'user not found in db')
             return null
+          }
+
+          const token = randomUUID()
+          await prisma.session.create({
+            data: {
+              userId: user?.id,
+              expires: new Date(Date.now() + maxAge * 1000),
+              sessionToken: token,
+           }
+          })
+          return {
+            id: user?.id,
+            username: user?.username,
+            email: user?.email,
+            name: user?.name,
+            image: user?.image,
+            sessionToken: token,
           }
         } catch(error) {
           console.log(error)
@@ -77,6 +93,46 @@ export const options: NextAuthOptions = {
       }
     })
   ],
+  session: {
+    strategy: "jwt",
+    maxAge: maxAge, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
+  callbacks: {
+    // temporary solution
+    async jwt({ token, user }: any) {
+      
+      if (user) {
+          console.log(user, 'test1')
+        token.userId = user.id
+        token.sessionToken = user.sessionToken
+        token.username = user.username
+      }
+      if (token?.sessionToken) {
+        const session = await prisma.session.findFirst({
+          where: {
+            sessionToken: token.sessionToken,
+          },
+          include: {
+            user: true,
+          }
+        })
+        if (!session) {
+          return null
+        }
+      }
+      console.log(token, 'test')
+      return token
+    },
+    session: ({ session, token }) => ({
+        ...session,
+        user: {
+            ...session.user,
+            id: token?.userId,
+            username: token?.username
+        },
+    }),
+}
 //   pages: {
 //     signIn: '/login'
 //   }
