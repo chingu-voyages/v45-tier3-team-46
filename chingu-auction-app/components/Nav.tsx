58738a5b12c4b069dbf3e04b0390c28c9b47b9db
Curt <121463645+curtwl@users.noreply.github.com>
2023-08-23 22:33:21 -0500

    Display username or name depending on signup method

--- chingu-auction-app/components/Nav.tsx
+++ chingu-auction-app/components/Nav.tsx
@@ -1,7 +1,7 @@
 import Logo from '../public/assets/images/chingu_logo.png'
 import Image from 'next/image'
 
-const Nav = (props, session) => {
+const Nav = (props) => {
   console.log(props.session, 'nav session log')
   return (
     <div>
@@ -22,7 +22,7 @@ const Nav = (props, session) => {
             <div className='flex items-center lg:order-2'>
               {props.session ? (
                 <p className='text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800'>
-                  {props.test.split(' ')[0]}
+                  {props.test}
                 </p>
               ) : ( 
                 <>
