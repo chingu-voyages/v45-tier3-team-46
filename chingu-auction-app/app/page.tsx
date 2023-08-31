5780d9e9b4b28f27ea04e41aa324778e0b267754
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-31 16:59:19 -0500

    deleted nextUI provider

--- chingu-auction-app/app/page.tsx
+++ chingu-auction-app/app/page.tsx
@@ -1,8 +1,5 @@
-'use client'
-import { NextUIProvider } from '@nextui-org/react'
 import { AuctionCard } from '../components/AuctionCard'
 import Link from 'next/link'
-
 const sampleAuctions = [
   {
     id: 1,
@@ -48,41 +45,39 @@ const sampleCategories = [
 
 export default function Home() {
   return (
-    <NextUIProvider>
-      <main className=''>
-        <div className='p-5'>
-          <div className='w-full flex flex-row justify-between mb-3'>
-            <h1 className='font-bold text-4xl'>Recently Created Auctions</h1>
-            <button className='border px-3 rounded-md bg-blue-300'>
-              All Auctions
-            </button>
-          </div>
-          <div className='flex flex-wrap justify-around'>
-            {sampleAuctions.map((auction) => (
-              <AuctionCard
-                key={auction.id}
-                itemName={auction.itemName}
-                sellerName={auction.sellerName}
-                itemCondition={auction.itemCondition}
-                currentBid={auction.currentBid}
-                amountBids={auction.ammountBids}
-                buyPrice={auction.buyPrice}
-                timeLeft={auction.timeLeft}
-              />
+    <main className=''>
+      <div className='p-5'>
+        <div className='w-full flex flex-row justify-between mb-3'>
+          <h1 className='font-bold text-4xl'>Recently Created Auctions</h1>
+          <button className='border px-3 rounded-md bg-blue-300'>
+            All Auctions
+          </button>
+        </div>
+        <div className='flex flex-wrap justify-around'>
+          {sampleAuctions.map((auction) => (
+            <AuctionCard
+              key={auction.id}
+              itemName={auction.itemName}
+              sellerName={auction.sellerName}
+              itemCondition={auction.itemCondition}
+              currentBid={auction.currentBid}
+              amountBids={auction.ammountBids}
+              buyPrice={auction.buyPrice}
+              timeLeft={auction.timeLeft}
+            />
+          ))}
+        </div>
+        <div className='m-3 w-full border'>
+          <h1 className='font-bold text-4xl'>Search By Categories</h1>
+          <div className='text-center flex flex-wrap w-full border justify-between'>
+            {sampleCategories.map((item: string) => (
+              <Link key={item} href={`/auctions?${item}`}>
+                {item}
+              </Link>
             ))}
           </div>
-          <div className='m-3 w-full border'>
-            <h1 className='font-bold text-4xl'>Search By Categories</h1>
-            <div className='text-center flex flex-wrap w-full border justify-between'>
-              {sampleCategories.map((item: string) => (
-                <Link key={item} href={`/auctions?${item}`}>
-                  {item}
-                </Link>
-              ))}
-            </div>
-          </div>
         </div>
-      </main>
-    </NextUIProvider>
+      </div>
+    </main>
   )
 }
