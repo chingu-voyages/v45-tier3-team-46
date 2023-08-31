1d01ba8240f89c58c24af3231ba3078ecd4f98f1
Curt <121463645+curtwl@users.noreply.github.com>
2023-08-31 16:16:43 -0500

    Add page stubs for adding and editing addresses

--- chingu-auction-app/app/user/[userId]/shipping/page.tsx
+++ chingu-auction-app/app/user/[userId]/shipping/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ShippingForm from '@/components/ShippingForm'
+import Link from 'next/link'
 
 const Shipping = () => {
 
   return (
     <div>
       <h1 className="text-xl font-semibold mb-4">Edit Shipping and Billing Details</h1>
+      <Link href={`./shipping/addaddress`}>
+        Add an Address
+      </Link>
       <ShippingForm addressType={'Shipping'}/>
       <ShippingForm addressType={'Billing'}/>
     </div>
