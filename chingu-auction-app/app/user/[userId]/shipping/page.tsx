ca48b2c2c6bef7836f3a9ec171858b1fb851f134
Curt <121463645+curtwl@users.noreply.github.com>
2023-08-31 21:28:28 -0500

    Convert ShippingForm to NextUI

--- chingu-auction-app/app/user/[userId]/shipping/page.tsx
+++ chingu-auction-app/app/user/[userId]/shipping/page.tsx
@@ -7,11 +7,16 @@ const Shipping = () => {
   return (
     <div>
       <h1 className="text-xl font-semibold mb-4">Edit Shipping and Billing Details</h1>
-      <Link href={`./shipping/addaddress`}>
-        Add an Address
-      </Link>
-      <ShippingForm addressType={'Shipping'}/>
-      <ShippingForm addressType={'Billing'}/>
+      <div>
+        <Link href={`./shipping/addaddress`} as="./shipping/addaddress?type=Shipping">
+          <h2 className="text-xl font-semibold mb-4">Add a Shipping Address</h2>
+        </Link>
+      </div>
+      <div>
+        <Link href={`./shipping/addaddress`} as="./shipping/addaddress?type=Billing">
+          <h2 className="text-xl font-semibold mb-4">Add a Billing Address</h2>
+        </Link>
+      </div>
     </div>
   )
 }
--- chingu-auction-app/components/ShippingForm.tsx
+++ chingu-auction-app/components/ShippingForm.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import { useState } from 'react'
 import { useParams } from 'next/navigation'
+import { Input, Button } from "@nextui-org/react"
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 
 const ShippingForm = ({ addressType }) => {
   const [addresses, setAddresses] = useState([])
@@ -14,8 +17,11 @@ const ShippingForm = ({ addressType }) => {
     addressType: '',
   })
 
+  const searchParams = useSearchParams()
+  const search = searchParams.get('type')
+
   const {userId} = useParams()
-  console.log(userId)
+  console.log(userId, search)
 
   const addAddress = () => {
       setAddresses([...addresses, {}])
@@ -23,6 +29,7 @@ const ShippingForm = ({ addressType }) => {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value }: { name: string, value: string } = event.target
+    console.log(formData)
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }))
   }
 
@@ -51,86 +58,41 @@ const ShippingForm = ({ addressType }) => {
 
   return (
     <div>
-      <h3 className='text-xl font-semibold mb-4'>{addressType} Addresses</h3>
-      {addresses.map((address, index) => (
-        <div key={index} className="mb-2">
-          <button
-            onClick={() => setAddresses(addresses.filter((_, i) => i !== index))}
-            className='text-red-600 hover:text-red-800'
-          >
-            -
-          </button>
-          <div className='mt-4 p-4 bg-gray-100 rounded-lg grid grid-cols-1'>
-            <h3 className='text-lg font-medium mb-2'>Address {index + 1}</h3>
-            <form className='grid grid-cols-1'>
-              <div className="mt-2">
-                <label htmlFor={`street${index}`} className='block mb-1'>
-                  Street 1
-                </label>
-                <input
-                  type='text'
-                  id={`street1${index}`}
-                  name={'street1'}
-                  value={formData.street1}
-                  onChange={handleChange}
-                  className='block text-sm font-semibold leading-6 text-gray-900'
-                />
-                <label htmlFor={`street${index}`} className="block mb-1">
-                  Street 2
-                </label>
-                <input
-                  type='text'
-                  id={`street2${index}`}
-                  name={'street2'}
-                  value={formData.street2}
-                  onChange={handleChange}
-                  className='block text-sm font-semibold leading-6 text-gray-900'
-                />
-                <label htmlFor={`city${index}`} className="block mb-1">
-                  City
-                </label>
-                <input
-                  type='text'
-                  id={`city${index}`}
-                  name={'city'}
-                  value={formData.city}
-                  onChange={handleChange}
-                  className='block text-sm font-semibold leading-6 text-gray-900'
-                />
-                <label htmlFor={`city${index}`} className='block mb-1'>
-                  State
-                </label>
-                <input
-                  type='text'
-                  id={`state${index}`}
-                  name={'state'}
-                  value={formData.state}
-                  onChange={handleChange}
-                  className='block text-sm font-semibold leading-6 text-gray-900'
-                />
-                <label htmlFor={`city${index}`} className='block mb-1'>
-                  Zip
-                </label>
-                <input
-                  type='text'
-                  id={`zip${index}`}
-                  name={'zip'}
-                  value={formData.zip}
-                  onChange={handleChange}
-                  className='block text-sm font-semibold leading-6 text-gray-900'
-                />
-                <button onClick={handleSubmit}>Submit</button>
-              </div>
-            </form>
-          </div>
+      <div className="flex flex-col space-y-4 items-center">
+      <h1 className="mt-4 mb-8 text-3xl">Add a {search} Address</h1>
+      <form className="grid grid-cols-2 gap-4 max-w-xl mx-auto" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="street1" className="block text-sm font-medium text-gray-700">Street 1</label>
+          <Input type="text" id="street1" name="street1" value={formData.street1} onChange={console.log('test')} className="mt-1" />
         </div>
-      ))}
-      <button
-        onClick={addAddress}
-        className='text-gray-600 hover:text-gray-800'
-      >
-        +
-      </button>
+        <div>
+          <label htmlFor="street2" className="block text-sm font-medium text-gray-700">Street 2</label>
+          <Input type="text" id="street2" name="street2" value={formData.street2} onChange={handleChange} className="mt-1" />
+        </div>
+        <div>
+          <label htmlFor="city" className="block text-sm font-medium text-gray-700">City</label>
+          <Input type="text" id="city" name="city" value={formData.city} onChange={handleChange} className="mt-1" />
+        </div>
+        <div>
+          <label htmlFor="state" className="block text-sm font-medium text-gray-700">State</label>
+          <Input type="text" id="state" name="state" value={formData.state} onChange={handleChange} className="mt-1" />
+        </div>
+        <div>
+          <label htmlFor="zip" className="block text-sm font-medium text-gray-700">Zip</label>
+          <Input type="text" id="zip" name="zip" value={formData.zip} onChange={handleChange} className="mt-1" />
+        </div>
+        <div className="col-span-2 grid grid-cols-2 gap-4">
+          <Button color="primary" className="w-full mt-2" type="submit">
+            Add Address
+          </Button>
+          <Link href={`../shipping`}>
+            <Button color="danger" className="w-full mt-2">
+              Cancel
+            </Button>
+          </Link>
+        </div>
+      </form>
+      </div>
     </div>
   )
 }
