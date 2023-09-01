0d39cade810f6056c7ee61e531758e6b42e36fd0
Curt <121463645+curtwl@users.noreply.github.com>
2023-09-01 17:18:30 -0500

    Add Addresses component with NextUI tabs

---
+++ chingu-auction-app/components/Addresses.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+import {Tabs, Tab, Card, CardBody, Button} from "@nextui-org/react"
+
+const Addresses = ({addresses, type}) => {
+//   const {userId} = useParams()
+
+  console.log(addresses)
+  //const billingAddresses = fetch()
+  const tabsJSX = addresses?.map((address, index) => (
+    <Tab 
+      key={`address${index}`} 
+      title={index === 0 ? `Default` : `Address ${index+1}`}>
+    <Card className="w-64">
+      <CardBody>
+        <p>{address.street1}</p>
+        <p>{address.street2}</p>
+        <p>{address.city}, {address.state} {address.zip}</p>
+      </CardBody>
+    </Card>  
+    </Tab>
+  ))
+
+  return (
+      <div className="flex w-full flex-col">
+      <Tabs aria-label="Options">
+        {tabsJSX}
+      </Tabs>
+      <div className="flex ">
+      <Link href={`./shipping/addaddress`} as={`./shipping/addaddress?type=${type}`}>
+        <Button color="primary" className="w-1/10 mb-8 mr-4">Add Address</Button>
+        </Link>
+        <Button color="primary" className="w-1/10 mb-8">Edit Address</Button>
+      </div>
+    </div>  
+  )
+}
+
+export default Addresses
