5b3b7a8d8cd1c2b63792c0c16e137405c59e0111
Curt <121463645+curtwl@users.noreply.github.com>
2023-09-08 19:40:15 -0500

    Implement endpoint to check for and handle ended auctions

---
+++ chingu-auction-app/app/api/checkauctions/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from "next/server"
+import prisma from "@/lib/prisma"
+
+export async function GET(req: Request) {
+  const now = new Date()
+  const endedAuctions = await prisma.item.findMany({
+    where: {
+      expiresAt: {
+        lt: now
+      },
+      auctionEnded: false,
+    },
+    include: {
+      bids: {
+        orderBy: {
+          bidAmount: 'desc',
+        },
+      },
+    },
+  })
+
+  endedAuctions.map(async (auctionItem) => {
+    if (auctionItem.bids.length > 0) {
+      const highestBid = await prisma.bid.findFirst({
+        where: { itemId: auctionItem?.id },
+        orderBy: { bidAmount: 'desc' },
+      })
+      await prisma.bid.update({
+        where: { id: highestBid?.id },
+        data: { won: true }
+      })
+      await prisma.item.update({
+        where: {
+          id: auctionItem.id,
+        },
+        data: {
+          purchasedBy: { 
+            connect: { 
+              id: highestBid?.bidderId,
+            },
+          },
+          auctionEnded: true,
+          sold: true,
+        }
+      })
+      // send notification to buyer?
+    } else {
+      await prisma.item.update({
+        where: {
+          id: auctionItem.id,
+        },
+        data: {
+          auctionEnded: true,
+        }
+      })
+    }
+  })
+
+  return NextResponse.json(endedAuctions, { status: 200 })
+}
